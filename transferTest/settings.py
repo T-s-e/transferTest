1b3c459b518ebb312588a05c95b31f061988f051
XKX <xie.kessin@gmail.com>
2022-06-16 10:29:30 +0800

    SECRET_KEY

--- transferTest/settings.py
+++ transferTest/settings.py
@@ -20,7 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^e(tao*lgu^2r#81edb$8!*03)akn$)-$mmvye^ate29z#iw0'
+# SECRET_KEY = 'django-insecure-g^e(tao*lgu^2r#81edb$8!*03)akn$)-$mmvye^ate29z#iw0'
+
+# ...
+
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
